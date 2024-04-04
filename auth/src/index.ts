83214d84efa1a73b616ae911d1a34c7c07a04ff3
navhen <esha.vista@gmail.com>
2024-04-05 03:58:44 +0700

    - End Section 7 - Start Section 8

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { json } from 'body-parser';
+import 'express-async-errors';
 
 import { currentUserRouter } from './routes/current-users';
 import { signinRouter } from './routes/signin';
 import { singoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handlers';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -12,8 +15,12 @@ app.use(json());
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(singoutRouter);
-app.use(signupRouter)
+app.use(signupRouter);
 
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+app.use(errorHandler);
 
 
 /* // section 5 setup
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,32 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
-router.get('/api/users/signup', (req, res) => {
-    res.send('Router signup');
+router.post('/api/users/signup', [
+    body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+    body('password')
+        .trim()
+        .isLength({min: 4, max: 20})
+        .withMessage('Password must be between 4 and 20 character')
+    ], 
+    async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        // this is javascript land (not ts)
+        throw new RequestValidationError(errors.array());
+    }
+    // 
+    //const { email, password } = req.body;
+
+    console.log('Creating a user...');
+    throw new DatabaseConnectionError();
+    res.send({});
 
 });
 
